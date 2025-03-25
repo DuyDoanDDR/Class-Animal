3d9aea613c447fa08f051d20c347b2ecdbe2b67c
Doan Minh Duy <duydoan812@gmail.com>
2025-03-26 00:45:00 +0700

    Update Commit

--- Class Animal/Program.cs
+++ Class Animal/Program.cs
@@ -14,18 +14,18 @@
     public abstract void PrintInfo(); //abstact method la phuong thuc khong co phan than, chi duoc khai bao nhung khong duoc dinh nghia ben trong abstract class
                                       //No bat buoc phai duoc override trong cac lop con (derived class)
 }
-class Cat : Animal
+class Cat : Animal //Class Cat ke thua cac thuoc tinh cua class Animal
 {
-    private string Name { get; set; }
-    public Cat(string weight, string height, string name)
-    : base(weight, height)
+    private string Name { get; set; } //Property rieng ten la Name, chi duoc truy cap ben trong class cat
+    public Cat(string weight, string height, string name) //Phuong thuc khoi tao cho Class Cat, 
+    : base(weight, height) //Tham chieu den phuong thuc khoi tao cua Class Animal va truyen weight va height len no
     {
         this.Name = name;
     }
 
-    public override void PrintInfo()
+    public override void PrintInfo() //Ghi de len phuong thuc PrintInfo
     {
-       Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + "Name: {2}", this.weight, this.height, this.Name);
+       Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + "Name: {2}", this.weight, this.height, this.Name); //Noi dung ghi de va thuoc tinh se duoc ghi len
     }
 }
 public class Program
